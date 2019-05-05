1bb64bbf0421a25ad2193e96550457749a6ffc7e
Sandip Guchait <35764971+sandipguchait@users.noreply.github.com>
2019-05-05 17:45:27 +0530

    completing Home page Banner and Error page banner

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import Hero from '../components/Hero';
+import Banner from '../components/Banner';
+import {Link} from 'react-router-dom';
 
 const Error = (props) => {
     return (
-       <Hero/>
+        <Hero>
+            <Banner title="404"
+                    subtitle="Page not found">
+                <Link to="/" className="btn-primary">Return Home</Link> 
+            </Banner>
+        </Hero>
     );
 };
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import Hero from '../components/Hero';
-
+import Banner from '../components/Banner';
+import {Link} from 'react-router-dom';
 
 const Home = (props) => {
     return (
-        <div>
-            <Hero />
-        </div>
+        <Hero>
+            <Banner title="Luxurious rooms"
+                    subtitle="deluxe rooms starting at $299">
+                <Link to="/rooms" className="btn-primary">Our Rooms</Link> 
+            </Banner>
+        </Hero>
     );
 };
 
